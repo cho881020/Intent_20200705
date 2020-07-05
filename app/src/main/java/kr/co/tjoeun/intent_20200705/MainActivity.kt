3a4f0f99b5ef57c66d984684c9f3584a57f3d9cb
조경진 <kj_cho@idealidea.co.kr>
2020-07-05 14:11:42 +0900

    돌아온 화면에서 데이터를 받아서 적용  - onActivityResult 함수 오버라이딩

--- app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.intent_20200705
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -52,4 +53,39 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+//    MainActivity로 복귀하는 모든 시점에 실행되는 함수.
+//    Ex. 닉네임을 받으러 갔다 => OK로 돌아와도 실행
+//    Ex. 닉네임을 받으러 갔다 => 취소로 돌아와도 실행
+//    Ex. 비번을 변경하러 갔다가 => OK 로 돌아와도 실행.
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        어떤걸 가지러 다녀온건지 확인 (닉네임 인지)
+        if (requestCode == 1000) {
+
+//            완료를 누른게 맞는지
+            if (resultCode == Activity.RESULT_OK) {
+
+//                결과로 받아온 닉네임을 텍스트뷰에 적용
+                val newNickName = data?.getStringExtra("nick")
+
+                nickNameTxt.text = newNickName
+
+            }
+
+        }
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
